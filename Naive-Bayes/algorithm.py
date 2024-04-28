34eee97aa0e1b1637a2e8d76e374f7ef3f274d0d
Gun Ho, Choi <54893409+longinus99@users.noreply.github.com>
2024-04-28 22:00:26 +0900

    Update algorithm.py

--- Naive-Bayes/algorithm.py
+++ Naive-Bayes/algorithm.py
@@ -1,4 +1,4 @@
-#naive_bayes
+#naive_bayes + kkma
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
